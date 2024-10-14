6dbd6f630bf49366195ef682de1893a99b370fe5
furqhan bin moosa <furqhanbinmoosaptr@gmail.com>
2024-10-14 19:19:48 +0530

    service modals added

--- src/pages/Home/MyServices/index.js
+++ src/pages/Home/MyServices/index.js
@@ -12,9 +12,6 @@ import { Link } from "react-router-dom";
 
 
 
-// comm
-
-
 import MigrationAndModernization from "../../../assets/img/Migration_Modernization.png" 
 import CloudAutiomation from "../../../assets/img/Cloud_Automation.png" 
 import SRE from "../../../assets/img/Cloud_SRE (1).png" 
@@ -23,12 +20,209 @@ import Serverless from "../../../assets/img/serverless.jpg"
 import securityComplianceManagement from "../../../assets/img/security-compliance-management.jpg" 
 
 
+class MigrationAndModernizationModal extends Component {
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="xl"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+     <div>
+     <div className="main-container d-block w-100 px-lg-5 px-md-3 px-2">
+        <Container fluid>
+          <div className="d-block w-100 text-start pb-4 pt-4 main-heading">
+            Microservices <span>Transformation in Cloud</span>
+          </div>
+          {/* <div className="d-block w-100 text-center pb-5 service-banner"> */}
+            {/* <img src={ServiceImage} alt="" /> */}
+          {/* </div> */}
+          <div className="d-block w-100 service-contents">
+           
+            <div className="d-block w-100 mb-4 content">
+            <p>Whether you're aiming to streamline your applications, or modernize your IT architecture, I offer tailored solutions that align with your goals.</p>
+             
+            </div>
+            <div className="d-block w-100 mb-4 content">
+              <h4 className="d-block w-100 pb-3 mb-3">key services includes:
+              </h4>
+            <ul>
+              <li>Containerization and Serverless Transformation</li>
+              <li>Monolith to Microservices Transformation
+              </li>
+              <li>Modern ultrasmart Cloudnative App development
+              </li>
+              <li>Data Lifecycle Management
+              </li>
+              <li>IOT and Edge Computing
+              </li>
+            </ul>
+            </div>
+            <div className="d-block w-100 mb-4 content">
+              <h4 className="d-block w-100 pb-3 mb-3">Accelerators</h4>
+              <p>Please look at my Opensource Projects
+              </p>
+           <ol>
+            <li>Appblocks technology that reduce any cloudnative enterprise product development or transformation effort by 60% <br /> (Link to  <a target="_blank" href="https://github.com/ptrsoft#appblocks-framework">https://github.com/ptrsoft#appblocks-framework</a>  )</li>
+            <li> A cloudnative Automation framework that reduce Opex by 50% <br />
+            (Link to <a target="_blank" href="https://github.com/ptrsoft#automation-framework"> https://github.com/ptrsoft#automation-framework</a>)</li>
+           </ol>
+           <p>Ready to transform your IT systems? Let’s connect(link) and discuss how I can help your business thrive.</p>
+            </div>
+          
+            
+          </div>
+        </Container>
+        {/* <AwsomeProject /> */}
+      </div>
+     </div>
+        <Modal.Footer>
+          <Button onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+class CloudAutomationModal extends Component {
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="xl"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+     <div>
+     <div className="main-container d-block w-100 px-lg-5 px-md-3 px-2">
+        <Container fluid>
+          <div className="d-block w-100 text-start pb-4 pt-4 main-heading">
+          Cloud <span>Automation</span>
+          </div>
+          {/* <div className="d-block w-100 text-center pb-5 service-banner"> */}
+            {/* <img src={ServiceImage} alt="" /> */}
+          {/* </div> */}
+          <div className="d-block w-100 service-contents">
+           
+            <div className="d-block w-100 mb-4 content">
+<p>I offer customized solutions to automate your cloud environment, reducing manual interventions, improving efficiency, and enabling seamless scalability.</p>             
+            </div>
+            <div className="d-block w-100 mb-4 content">
+              <h4 className="d-block w-100 pb-3 mb-3">key services includes:
+              </h4>
+            <ul>
+             <li>Infrastructure as Code (IaC)
+             </li>
+             <li>CI/CD pipelines
+             </li>
+             <li>Automated Monitoring
+             </li>
+            </ul>
+            </div>
+            <div className="d-block w-100 mb-4 content">
+              <h4 className="d-block w-100 pb-3 mb-3">Accelerators</h4>
+           <ul>
+            <li> A cloudnative Automation framework that reduce Opex by 50% <br />
+            (Link to <a target="_blank" href="https://github.com/ptrsoft#automation-framework"> https://github.com/ptrsoft#automation-framework)</a></li>
+            <li> A SRE framework that reduce operational dashboarding and alerting efforts by Autodiscovery of Infrastructure and Services and Autoprovisioning of Dashboards & Alerts <br /> 
+            (Link to <a target="_blank" href="https://github.com/ptrsoft#automation-framework">https://github.com/ptrsoft#automation-framework</a> )</li>
+            <li> A Serverless UI deployment framework that deploy Web and Mobility Applications in One Go with 90% Reduced Capex. <br />(Link to <a target="_blank" href="https://github.com/ptrsoft#automation-framework">https://github.com/ptrsoft#automation-framework</a>)</li>
+           </ul>
+           <p>Ready to take your cloud infrastructure to the next level? Let’s connect and discuss how I can transform your cloud operations.</p>
+            </div>
+          
+          </div>
+        </Container>
+        {/* <AwsomeProject /> */}
+      </div>
+     </div>
+        <Modal.Footer>
+          <Button onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+class SREModal extends Component {
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="xl"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+     <div>
+     <div className="main-container d-block w-100 px-lg-5 px-md-3 px-2">
+        <Container fluid>
+          <div className="d-block w-100 text-start pb-4 pt-4 main-heading">
+          System Reliability <span>Engineering</span> 
+          </div>
+          {/* <div className="d-block w-100 text-center pb-5 service-banner"> */}
+            {/* <img src={ServiceImage} alt="" /> */}
+          {/* </div> */}
+          <div className="d-block w-100 service-contents">
+           
+            <div className="d-block w-100 mb-4 content">
+              <p>My expertise focuses on improving system availability, performance,resilience, and reliability through proactive monitoring, automation.</p>
+            </div>
+            <div className="d-block w-100 mb-4 content">
+              <h4 className="d-block w-100 pb-3 mb-3">key services includes:
+              </h4>
+            <ul>
+            <li>Infrastructure Monitoring
+            </li>
+            <li>Container and Serverless Monitoring
+            </li>
+            <li>ML-Ops
+            </li>
+            </ul>
+            </div>
+            <div className="d-block w-100 mb-4 content">
+              <h4 className="d-block w-100 pb-3 mb-3">Accelerators</h4>
+           <ul>
+            <li> A mechanism for auto-discovery of distributed microservices and auto enablement of their operation dashboard and alerting systems that reduce operation management effort 70%.</li>
+            <li>Extendible SRE framework based on small plugin based golang CLI tooling that increased productivty by 50%.
+            </li>
+
+           </ul>
+           <p>From infrastructure automation and monitoring to root cause analysis and optimization, I provide end-to-end solutions tailored to your business needs, keeping your systems running smoothly, even in high-demand scenarios.</p>
+           <p>Ready to improve your system’s reliability and scalability? Let’s connect and discuss how I can help ensure your systems are always running at their best.</p>
+            </div>
+          
+          </div>
+        </Container>
+        {/* <AwsomeProject /> */}
+      </div>
+     </div>
+        <Modal.Footer>
+          <Button onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
 
 class MyServices extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { modalShow: false, cloudModalShow: false , SREModalShow: false};
+
   }
+
+  setModalShow = (show) => {
+    this.setState({ modalShow: show });
+  }; 
+   setCloudModalShow = (show) => {
+    this.setState({ cloudModalShow: show });
+  };
+
+  setSREModalShow = (show) => {
+    this.setState({ SREModalShow: show });
+  };
+
 
   handleScrollToTop = () => {
     window.scrollTo(0, 0);
@@ -36,8 +230,8 @@ class MyServices extends Component {
 
   render() {
     var settings = {
-      dots: true,
-      infinite: true,
+      // dots: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -99,9 +293,9 @@ class MyServices extends Component {
           <Slider {...settings} className="mb-5">
             <div className="d-block w-100 services-box">
               <Link
-                to="/service/migration-modernization"
-                onClick={this.handleScrollToTop}
-              >
+                // to="/service/migration-modernization"
+                onClick={() => this.setModalShow(true)}
+                >
                 <img
                   src={SafeImage}
                   alt="Migration and Modernization"
@@ -126,8 +320,9 @@ class MyServices extends Component {
               </Link>
               <div className="button">
                 <Link
-                  to="/service/migration-modernization"
-                  onClick={this.handleScrollToTop}
+                  // to="/service/migration-modernization"
+                  // onClick={this.handleScrollToTop}
+                  onClick={() => this.setModalShow(true)}
                 >
                   <Button>
                     <i className="fa-solid fa-arrow-right"></i>
@@ -138,8 +333,9 @@ class MyServices extends Component {
 
             <div className="d-block w-100 services-box">
               <Link
-                to="/service/cloud-automation"
-                onClick={this.handleScrollToTop}
+                // to="/service/cloud-automation"
+                // onClick={this.handleScrollToTop}
+                onClick={this.setCloudModalShow}
               >
                 <img
                   src={SafeImage}
@@ -164,9 +360,11 @@ class MyServices extends Component {
               </Link>
               <div className="button">
                 <Link
-                  to="/service/cloud-automation"
-                  onClick={this.handleScrollToTop}
+                  // to="/service/cloud-automation"
+                  // onClick={this.handleScrollToTop}
+                                  onClick={this.setCloudModalShow}
                 >
+
                   <Button>
                     <i className="fa-solid fa-arrow-right"></i>
                   </Button>
@@ -176,8 +374,10 @@ class MyServices extends Component {
 
             <div className="d-block w-100 services-box">
               <Link
-                to="/service/system-reliability-engineering"
-                onClick={this.handleScrollToTop}
+                // to="/service/system-reliability-engineering"
+                // onClick={this.handleScrollToTop}
+                onClick={this.setSREModalShow}
+
               >
                 <img
                   src={SafeImage}
@@ -202,8 +402,8 @@ class MyServices extends Component {
               </Link>
               <div className="button">
                 <Link
-                to="/service/system-reliability-engineering"
-                onClick={this.handleScrollToTop}
+                // to="/service/system-reliability-engineering"
+                onClick={this.setSREModalShow}
                 >
                   <Button>
                     <i className="fa-solid fa-arrow-right"></i>
@@ -327,6 +527,17 @@ class MyServices extends Component {
             </div>     */}
           </Slider>
         </Container>
+        <MigrationAndModernizationModal
+          show={this.state.modalShow}
+          onHide={() => this.setModalShow(false)}
+        />
+         <CloudAutomationModal
+          show={this.state.cloudModalShow}
+          onHide={() => this.setCloudModalShow(false)}
+        /> <SREModal
+          show={this.state.SREModalShow}
+          onHide={() => this.setSREModalShow(false)}
+        />
       </div>
     );
   }
