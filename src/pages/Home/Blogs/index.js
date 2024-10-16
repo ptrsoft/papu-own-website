504462c8eead4ff4d95fbcba97f93d1e7d40770c
furqhan bin moosa <furqhanbinmoosaptr@gmail.com>
2024-10-16 14:32:46 +0530

    portdolio and github navigations added

---
+++ src/pages/Home/Blogs/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <div id='blogs' className="blogs-container experince-container d-block w-100 py-lg-5 py-3 px-lg-5 px-3">
+  <h2 className="d-block w-100 text-center pb-md-5 mb-3 heading">
+          My <span>Blogs</span>
+        </h2>
+
+
+    </div>
+  )
+}
+
+export default Blogs
